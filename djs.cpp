738459b53623b2eab7edf8c3343405fe53d7aebb
David Jacobowitz <david.jacobowitz@gmail.com>
2015-04-22 15:53:16 -0700

    first commit

---
+++ djs.cpp
@@ -0,0 +1,335 @@
+#include "djs.h"
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+// private to djs
+bool       djs_trim(djs_tok_t &vt);
+const char *djs_type2str(djs_tok_t t);
+djs_tok_t  djs_createNull();
+bool       djs_strEql(const djs_tok_t kt, const char *ks);
+int        djs_fA2(const djs_tok_t st, bool find, const int idx,  djs_tok_t &vt);
+bool       djs_getBlob(const djs_tok_t st, djs_tok_t &left, djs_tok_t &right);
+
+
+djs_type_t 
+djs_detType(djs_tok_t it) {
+ if (!it.v) return _djs_na; 
+ djs_type_t rt = djs_naked;
+ int i = it.b;
+ bool done = false;
+ while (!done) {
+  if      (it.s[i] == '[') { rt = djs_array;  done = true; }
+  else if (it.s[i] == '{') { rt = djs_hash;   done = true; }
+  else if (it.s[i] == '"') { rt = djs_string; done = true; }
+  else if (isgraph(it.s[i])) done = true;
+  i++;
+  done |= (i == it.e);
+ }
+ it.t = rt;
+ // printf("type %s\n",djs_type2str(it));
+ return rt;
+};
+
+djs_tok_t djs_createFrom(const char *src) {
+ djs_tok_t ot = djs_createNull();
+ ot.s = src;
+ ot.e = strlen(src)-1;
+ ot.v = true;
+ ot.t = djs_detType(ot);
+ return ot;
+};
+
+djs_tok_t djs_createNull() {
+ djs_tok_t ot;
+ memset(&ot,0,sizeof(ot));
+ return ot;
+}
+
+void
+showRange(const char *s,int b, int e) {
+ for (int i=b;i<=e;i++) {
+  printf("%c",s[i]);
+ }
+};
+
+bool djs_strEql(const djs_tok_t kt, const char *ks) {
+ if (!kt.v)       return false;
+ int ksl = strlen(ks);
+ int ktl = kt.e - kt.b + 1;
+ if (!strlen(ks)) return false; 
+ if (ksl != ktl)  return false;
+ for (int i=0;i<ksl;i++) {
+  if (ks[i] != kt.s[kt.b+i]) return false;
+ } 
+ return true;
+};
+
+const char *djs_type2str(djs_tok_t t) {
+ return ((t.t == djs_hash)   ? "hash" :
+         (t.t == djs_string) ? "str " :
+	 (t.t == djs_array)  ? "arry" :
+	 (t.t == djs_naked)  ? "nakd" : "????");
+}
+
+void djs_showTok(const djs_tok_t t) {
+ if (t.v) {
+  printf("tok [%d,%d] (%s) >>",
+		  t.b,t.e,djs_type2str(t));
+  for (int i=t.b;i<=t.e;i++) {
+   printf("%c",t.s[i]);
+  }
+  printf("<<\n");
+ } else {
+  printf("tok [invalid]\n");
+ }
+};
+
+bool djs_trim(djs_tok_t &vt) {
+ int fix_count = 0;
+ if (vt.v) {
+  int i = vt.b;	  
+  bool done = false;
+  int qcount = 0;
+  while (!done) {
+   bool trimmable = !isgraph(vt.s[i]);
+   if (vt.t == djs_string) {
+    if (vt.s[i] == '"') {
+     qcount += 1;
+     if (qcount == 1) trimmable = true;
+    }
+   }
+   if (trimmable) vt.b++;
+   done |= (i == vt.e);
+   done |= !trimmable;
+   i++;
+  }
+  int j = vt.e;	  
+  done = false;
+  qcount = 0;
+  while (!done) {
+   bool trimmable = !isgraph(vt.s[j]);
+   // if (vt.t == djs_string) g &= (vt.s[j] != '"');
+   if (vt.t == djs_string) {
+    if (vt.s[j] == '"') {
+     qcount += 1;
+     // printf("qcount %d\n",qcount);
+     if (qcount == 1) { trimmable = true; }
+    }
+   }
+   if (trimmable) vt.e--;
+   done |= (j == vt.b);
+   done |= !trimmable;
+   j--;
+  }
+ }
+ return (fix_count > 0);
+}
+
+bool djs_findIndex(const djs_tok_t st, const int idx, djs_tok_t &vt) {
+ return djs_fA2(st, true, idx, vt);
+}
+int djs_getLength(const djs_tok_t st) {
+ djs_tok_t dummy;
+ return djs_fA2(st, false, 0, dummy);
+}
+
+bool
+djs_getBlob(const djs_tok_t st, djs_tok_t &left, djs_tok_t &right) {
+ left = st;
+ right = st;
+
+ if (st.v) {
+  char lastch = 0;
+  bool in_string = false;
+  int depth = 0;
+  int last_depth = 0;
+  for (int i=st.b;i<=st.e;i++) {
+   char inch = st.s[i];
+   //  if (lastch == '\\') continue;
+   if ((inch == '"') && (lastch != '\\'))  in_string = !in_string;
+   if (!in_string) {
+    if      (inch == '{') depth++;
+    else if (inch == '}') depth--;
+    else if (inch == '[') depth++;
+    else if (inch == ']') depth--;
+   }
+   if ((!in_string && ((inch == ',') && (depth == 0))) ||
+       ((depth == -1) && (last_depth == 0)))  {
+    left.e = i-1;
+    right.b = i+1;
+    if (left.e >= left.b) {
+     left.t = djs_detType(left);
+     //printf("blob before trim\n");
+     // djs_showTok(left);
+     djs_trim(left);
+     printf("blob after trim\n");
+     djs_showTok(left);
+     return true;
+    } else {
+
+    }
+   }
+   lastch = inch;
+   last_depth = depth;
+  }
+  left.v = false;
+  right.v = false;
+  return false;
+ }
+ return false;
+};
+
+
+int djs_fA2(const djs_tok_t st, bool find, const int idx , djs_tok_t &left) {
+ int vcount = 0;
+ djs_tok_t stc = st;
+ left = st;
+ left.v = false;
+ if (st.v && (st.t == djs_array)) {
+  while ((stc.s[stc.b] != '[') && (stc.b <= st.e))  stc.b++;
+  stc.b++;
+  djs_tok_t right;
+  while (djs_getBlob(stc,left,right)) {
+   //if (left.b != left.e) {
+   if (left.v) {
+    printf("incrementing vcount\n");
+    vcount++;
+    left.v = true;
+    djs_showTok(left);
+   }
+   if (find && ((vcount-1) == idx)) {
+    // left.t = djs_detType(left);
+    printf("we found:\n");
+    djs_showTok(left);
+    return 1;
+   }
+   stc.b = right.b;
+  }
+ }
+ return find ? 0 : vcount;
+}
+
+
+
+
+
+
+bool djs_findNamed(const djs_tok_t st, const char *ss, djs_tok_t &vt) {
+ int depth = 0;
+ bool in_string = 0;
+ djs_tok_t str = st;
+ djs_state_t cs = djs_st_start;
+ djs_state_t ns = cs;
+ djs_tok_t   kt = st;
+ djs_tok_t   left, right;
+ vt = st;
+ if (str.v && (str.t == djs_hash)) {
+  char lastch = 0;	  
+  for (int i=str.b;i<str.e;i++) {
+   char inch = str.s[i];
+   if (lastch == '\\') continue;
+   if (!in_string) {
+    if      (inch == '{') depth++;
+    else if (inch == '}') depth--;
+    else if (inch == '[') depth++;
+    else if (inch == ']') depth--;
+   }
+   switch (cs) {
+    case djs_st_start :
+    case djs_st_value_done :
+     if (!in_string && isgraph(inch) && (inch != '{')) {
+      if (inch == '"') in_string = true;
+      ns = djs_st_in_key;
+      int b = in_string ? (i+1) : i;
+      if (b <= str.e) {
+       kt.b      = b;
+       kt.e      = b;
+       vt.b      = 0;
+       vt.e      = 0;
+      } else {
+       return false;
+      }
+     }
+     break;
+    case djs_st_in_key :
+     if (in_string && (inch == '"')) {
+      in_string = false;
+      ns = djs_st_key_done;
+      kt.e = i-1;
+      printf("key is: ");
+      showRange(kt.s,kt.b,kt.e);
+      printf("\n");
+     } else if (!in_string && (inch == ':')) {
+      ns = djs_st_in_value; 
+      kt.e = i-1;
+      if (i+1 <= str.e) str.b = i+1;
+      else return false;
+      printf("key is: ");
+      showRange(kt.s,kt.b,kt.e);
+      printf("\n");
+     }
+     break;
+    case djs_st_key_done :
+     if (inch == ':') {
+      ns = djs_st_in_value;
+      if (i+1 <= str.e) str.b = i+1;
+      else return false; // malformed json
+     };
+     break;
+    case djs_st_in_value :
+     if (djs_getBlob(str,left,right)) {
+      ns = djs_st_value_done;
+      str = right;
+      i   = right.b;
+      if (djs_strEql(kt,ss)) {
+       vt = left;
+       vt.v = true;
+       vt.t = djs_detType(vt);
+       djs_trim(vt);
+       return true;
+      }
+     };
+     break;
+    default :
+     break;
+   }
+   if (0) printf("depth %d i %d cs %d ns %d ch %c\n",depth,i,cs,ns,inch);
+   cs = ns;
+   lastch = inch;
+  }
+ }
+ return false;
+};
+
+
+bool djs_getStr(const djs_tok_t t, char *s, int sl) {
+ int len = t.e-t.b+1;
+ if (len > (sl-1)) return false;
+ int i = 0;
+ while (i<len) {
+  s[i] = t.s[t.b+i];
+  i++;
+ }
+ s[i] = 0;
+ return true;
+};
+
+bool djs_getInt(const djs_tok_t t, int &v) {
+ // note this functon will try to convert a naked value
+ //  or a string. Obviously, the string still needs to look
+ //  like a number.
+ if ((t.t == djs_naked) || (t.t == djs_string)) {
+   char temp[20];
+   char *eptr;
+   djs_getStr(t,temp,20);
+   printf("temp is \"%s\"\n",temp);
+   v = strtol(temp,&eptr,10);
+   if (temp == eptr) return false; // checks if some of input was consumed
+   //if (*eptr == 0)   return false; // checks if all of input was consumed
+   return true;
+ }
+ return false;
+}
+
---
+++ test.cpp
@@ -0,0 +1,244 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "djs.h"
+
+void loadFile(char *dst, const char *fn) {
+ FILE *f = fopen(fn,"r");
+ int idx = 0;
+ while (!feof(f) && !ferror(f)) {
+  char c = getc(f);
+  // printf("idx %d c %c %2.2x\n",idx,c,c);
+  dst[idx++] = c;
+ }
+ dst[idx-1] = 0;
+ fclose(f);
+}
+
+int eI(int a, int b) {
+ if (a == b) {
+  return 0;
+ } else {
+  printf("FAIL %d != %d\n",a,b);
+ }
+ return 1;
+}
+
+int eS(const char *a, const char *b) {
+ if (!strcmp(a,b)) {
+  return 0;
+ } else {
+  printf("FAIL \"%s\" != \"%s\"\n",a,b);
+ }
+ return 1;
+} 
+
+int eT(int a) {
+ if (a) {
+  return 0;
+ } else {
+  printf("FAIL expected true\n");
+ }
+ return 1;
+}
+
+int eF(int a) {
+ if (!a) {
+  return 0;
+ } else {
+  printf("FAIL expected false\n");
+ }
+ return 1;
+}
+
+int tests_6() {
+ const char td1[] = "{aa:{b:{cc:{d:{ee:f}}}}}"; // not json, but we'll take it;
+ const char td2[] = "{\"aa\":{\"b\":{\"cc\":{\"d\":{\"ee\":\"f\"}}}}}";
+
+ int fc = 0;
+ djs_tok_t w1 = djs_createFrom(td1);
+ fc += eT(w1.v);
+ djs_tok_t w2 = djs_createFrom(td2);
+ fc += eT(w2.v);
+
+ const char *names[] = { "aa", "b", "cc", "d", "ee" };
+ djs_tok_t r1 = w1;
+ djs_tok_t r2 = w2;
+ for (int i=0;i<4;i++) {
+  bool ok;
+  djs_tok_t nr2;
+  fc += eT(ok = djs_findNamed(r2,names[i],nr2));
+  r2 = nr2;
+
+  djs_tok_t nr1;
+  fc += eT(ok = djs_findNamed(r1,names[i],nr1));
+  r1 = nr1;
+ }
+ return fc;
+}
+
+int tests_5() {
+
+ const char td[] = "[[[[[]]]]]";
+ djs_tok_t w = djs_createFrom(td);
+ int fc = 0;
+
+ fc += eT(w.v);
+ djs_tok_t a,b,c,d,e;
+ bool ok; 
+ fc += eT(ok = djs_findIndex(w,0,a));
+ fc += eT(ok = djs_findIndex(a,0,b));
+ fc += eT(ok = djs_findIndex(b,0,c));
+ fc += eT(ok = djs_findIndex(c,0,d));
+ fc += eF(ok = djs_findIndex(d,0,e));
+ return fc;
+};
+
+int tests_4() {
+
+ const int TSTRL = 128;
+ char tdata[2048];
+ char tstr[TSTRL];
+ int fc = 0;
+ bool ok = false;
+ djs_tok_t res;
+
+ loadFile(tdata,"inputs/ex3.json");
+
+ djs_tok_t w2 = djs_createFrom(tdata);
+ djs_showTok(w2);
+
+ const char *svals[] = {
+  "this",
+  "is",
+  "a",
+  "thing",
+  "with,commas,n'stuff,",
+  "and single quotes '",
+  "and escped double quotes -- singly \\\"",
+  "and in pairs \\\"ding\\\""
+ };
+
+ fc += eT(w2.v);
+ for (int i=0;i<8;i++) {
+  fc += eT(ok = djs_findIndex(w2,i,res));
+  fc += eT(ok = djs_getStr(res,tstr,TSTRL));
+  fc += eS(tstr,svals[i]);
+ }
+ return fc;
+}
+
+
+int tests_3() {
+
+ const int TSTRL = 128;
+ char tdata[2048];
+ char tstr[TSTRL];
+ int fc = 0;
+ bool ok = false;
+ djs_tok_t res;
+
+ loadFile(tdata,"inputs/ex2.json");
+
+ djs_tok_t w2 = djs_createFrom(tdata);
+ djs_showTok(w2);
+
+ const char *digits[] = {
+  "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"
+ };
+ const int ivals[]   = { 1, 2, 3, 4, 5, 6, 7, 8, 9, -10 };
+ const char *svals[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "[ 10 ]" };
+
+
+ fc += eT(w2.v);
+ for (int i=0;i<10;i++) {
+  fc += eT(ok = djs_findNamed(w2,digits[i],res));
+  fc += eT(ok = djs_getStr(res,tstr,TSTRL));
+  fc += eS(tstr,svals[i]);
+  int ival = 42;
+  fc += eI(djs_getInt(res,ival),(ivals[i] > 0));
+  fc += (ivals[i] > 0) ? eI(ivals[i], ival) : 42;
+ }
+ return fc;
+}
+
+
+int tests_2() {
+ const char td1[] = "{}";
+ const char td2[] = "[]";
+ int fc = 0;
+
+ djs_tok_t w1 = djs_createFrom(td1);
+ djs_tok_t w2 = djs_createFrom(td2);
+ djs_tok_t res;
+ bool ok = 0;
+
+ fc += eT(w1.v);
+ fc += eT(w2.v);
+ fc += eI(djs_getLength(w2),0);
+ fc += eF(ok = djs_findNamed(w1,"bob",res));
+ fc += eF(ok = djs_findNamed(w1,"",res));
+ fc += eF(ok = djs_findIndex(w2,0,res));
+ fc += eF(ok = djs_findIndex(w2,1,res));
+
+ return fc;
+};
+
+int tests_1() {
+ const int TSTRL = 128;
+ char tdata[2048];
+ char tstr[TSTRL];
+ int fc = 0;
+ bool ok = false;
+ djs_tok_t res, r3;
+ int aryLen = 0;
+
+ loadFile(tdata,"inputs/example.json");
+
+ djs_tok_t whole = djs_createFrom(tdata);
+
+ fc += eT(ok = djs_findNamed(whole, "this", res));
+ fc += eT(ok = djs_getStr(res,tstr,TSTRL));
+ fc += eS(tstr,"that");
+ fc += eT(ok = djs_findNamed(whole,"other",res));
+ fc += eT(ok = djs_findNamed(res,"this",res));
+ fc += eT(ok = djs_getStr(res,tstr,TSTRL));
+ fc += eS(tstr,"notthat");
+ fc += eT(ok = djs_findNamed(whole,"boo",res));
+ fc += eI(aryLen = djs_getLength(res),6);
+
+ bool intables[] = { true, true, false, true, false, true };
+ int  tvals[]    = { 11, 222, -1, 3333, -1, 55 };
+ for (int i=0;i<aryLen;i++) {
+  djs_tok_t r2;
+  fc += eT(ok = djs_findIndex(res,i,r2));
+  int ival;
+  fc += eI(djs_getInt(r2,ival),intables[i]);
+  if (intables[i]) {
+   fc += eI(ival, tvals[i]);
+  }
+ }
+
+ fc += eT(ok = djs_findNamed(whole,"poop",res));
+ fc += eT(ok = djs_findIndex(res,2,r3));
+ fc += eT(ok = djs_getStr(r3,tstr,TSTRL));
+ fc += eS(tstr, 
+   "{ \"in\": 2, \"st\": 333333, \"rs\": [ 3,3,3,3,3,3 ] }");
+
+ if (fc) {
+  printf("---- FAIL with %d errors ----\n",fc);
+ }
+
+ return fc;
+};
+
+int main(int argc, char *argv[]) {
+ tests_1();
+ tests_2();
+ tests_3();
+ tests_4();
+ tests_6();
+ return 0;
+
+};
+
